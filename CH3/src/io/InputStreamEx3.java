1740c96803cce40c72d6ae4b101f0489272aeea8
ronan10823 <louck10823@gmail.com>
2025-10-21 10:41:39 +0900

    IO_2

--- CH3/src/io/InputStreamEx3.java
+++ CH3/src/io/InputStreamEx3.java
@@ -8,12 +8,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class InputStreamEx1 {
+public class InputStreamEx3 {
     public static void main(String[] args) {
         // InputStream in = new FileInputStream(new File("null"));
-        try {
-            InputStream in = new FileInputStream("c:\\temp\\img1.jpg");
-            OutputStream out = new FileOutputStream("c:\\temp\\copy1.jpg");
+        try (InputStream in = new FileInputStream("c:\\temp\\img1.jpg");
+            OutputStream out = new FileOutputStream("c:\\temp\\copy1.jpg");) {
+            
 
             int input = 0;
             while ((input = in.read()) != -1) {
@@ -21,8 +21,6 @@ public class InputStreamEx1 {
                 out.write(input);
                 }
 
-            in.close();
-            out.close();
             
         } catch (IOException e) {  
             e.printStackTrace(); 
